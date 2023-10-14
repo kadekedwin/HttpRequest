8a568a72a32251fca5c071db75aeaa7876fc021a
kadekedwin <kadekedwin12@gmail.com>
2023-10-14 22:49:34 +0800

    add homeview and comment to contentview

---
+++ HttpRequest/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  HttpRequest
+//
+//  Created by Kadek Edwin on 14/10/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
